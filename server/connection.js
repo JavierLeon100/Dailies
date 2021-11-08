cac0bfc3efd346ef83e9e777e4cb5acc8c3a7c77
JavierLeon100 <lleonjjavier@gmail.com>
2021-11-08 09:58:18 -0800

    First Commit

--- server/connection.js
+++ server/connection.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-let mongoDB = `mongodb+srv://JavierLeon:JavierLeon100@cluster0.tzpqi.mongodb.net/Full-Stack-Project?retryWrites=true&w=majority`;
+let mongoDB = process.env.DB_CONNECTION;
 
 export default mongoose.connect(mongoDB); //returns a promise to app.js
